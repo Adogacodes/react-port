17831df99d74f55d97bd33cbfec7e4f4dea4b755
Adogacodes <107518703+Adogacodes@users.noreply.github.com>
2022-09-12 20:09:01 -0700

    React Portfolio (Tag rel added)

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,10 +3,10 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs';
 const SocialMedia = () => {
     return ( 
         <div className="app__social">
-            <a href='https://www.linkedin.com/in/isaac-adoga-315a2924a/' target='_blank'>
+            <a href='https://www.linkedin.com/in/isaac-adoga-315a2924a/' target='_blank' rel='noreferrer'>
                 <BsLinkedin />
             </a>
-            <a href='https://github.com/Adogacodes' target='_blank'>
+            <a href='https://github.com/Adogacodes' target='_blank' rel='noreferrer'>
                 <BsGithub />
             </a>
         </div>
